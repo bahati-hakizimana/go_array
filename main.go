6f71ddf20cecad21905ca7534e60d5f383ee20ed
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-03 07:28:13 -0700

    Go_Arra

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to array course")
+
+
+	var fruitList [4] string
+
+	fruitList[0]  = "Orange"
+	fruitList[1]  = "Apple"
+	fruitList[3]  = "Stron"
+
+
+	fmt.Println("Fruit list is:", fruitList)
+
+}
